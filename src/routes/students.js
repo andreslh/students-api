ccf499515babc16827080b38315bcf39ca31b593
Andrés Ledesma Higonet <andreslh90@gmail.com>
2022-04-15 00:04:05 -0300

    fix(public): images path updated and public folder accesible

--- src/routes/students.js
+++ src/routes/students.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer  = require('multer')
-const upload = multer({ dest: 'public/img/students/' })
+const upload = multer({ dest: 'public/images/students/' })
 
 const controller = require('../controllers/students');
 
